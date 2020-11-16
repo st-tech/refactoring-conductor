e236fe58b01eb80eda26d04af48decacbde9b160
yoshikawataiki <yoshikawataiki@gmail.com>
2020-11-16 13:58:51 +0900

    update pattern

--- internal/process_control.go
+++ internal/process_control.go
@@ -1,10 +1,22 @@
 package internal
 
-import "github.com/st-tech/search-tools/2020internship-yoshikawa/src/domain"
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/st-tech/search-tools/2020internship-yoshikawa/src/domain"
+)
 
 // CountControlFlow is to count Control Flow statements.
 func CountControlFlow(vbscript domain.VBScript, str string) int {
 	var cognitiveComplexityCount = 2
+
+	isIf, err := regexp.MatchString(domain.VBScriptControlFlowPattern, str)
+	if err != nil {
+		fmt.Printf("err: %v", err)
+	}
+
+	fmt.Printf("%v\n", isIf)
 
 	return cognitiveComplexityCount
 }
